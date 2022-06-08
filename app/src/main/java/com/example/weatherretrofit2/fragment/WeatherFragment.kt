c7d88ea13ed2b5c46e19a7efeeb924dc699d2c65
vasyavasya7628 <mineok44@gmail.com>
2022-06-08 21:05:26 +0700

    Думал почему погоду не получаю, оказывается нужен был vpn. Сейчас всё работает. Жду код ревью

--- app/src/main/java/com/example/weatherretrofit2/fragment/WeatherFragment.kt
+++ app/src/main/java/com/example/weatherretrofit2/fragment/WeatherFragment.kt
@@ -1,21 +1,27 @@
 package com.example.weatherretrofit2.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.weatherretrofit2.WeatherViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weatherretrofit2.data.DataWeather
+import com.example.weatherretrofit2.data.WeatherAdapter
+import com.example.weatherretrofit2.data.WeatherSealed
 import com.example.weatherretrofit2.databinding.FragmentWeatherBinding
 import timber.log.Timber
 
+private const val STATE_PLUS = 0
+private const val KEY: String = "1"
 
 class WeatherFragment : Fragment() {
 
     private var _binding: FragmentWeatherBinding? = null
     private val binding get() = _binding!!
     private val weatherViewModel: WeatherViewModel by viewModels()
+    private val weatherAdapter = WeatherAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,11 +34,29 @@ class WeatherFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        weatherViewModel.weatherExport.observe(viewLifecycleOwner){
-            Timber.d(it.toString())
+        weatherViewModel.weatherExport.observe(viewLifecycleOwner) {
+            loadDataToAdapter(it)
         }
+        initRecyclerView()
     }
-    fun loadDataToAdapter(){}
+
+    fun loadDataToAdapter(weatherDomain: List<DataWeather>) {
+        val list = mutableListOf<WeatherSealed>()
+        weatherDomain.map { weather ->
+            if (weather.temp > STATE_PLUS) {
+                list.add(WeatherSealed.WeatherPlus(weather))
+            } else {
+                list.add(WeatherSealed.WeatherMinus(weather))
+            }
+        }
+        Timber.d(list.toString())
+        weatherAdapter.submitList(list.toMutableList())
+    }
+
+    private fun initRecyclerView() {
+        binding.recyclerViewWeather.layoutManager = LinearLayoutManager(activity)
+        binding.recyclerViewWeather.adapter = weatherAdapter
+    }
 
 
     override fun onDestroyView() {
