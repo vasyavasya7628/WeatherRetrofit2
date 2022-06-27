b3a3db87209a8d968544a24a9f200a46d3f0c7c6
vasyavasya7628 <mineok44@gmail.com>
2022-06-27 13:21:44 +0700

    Замерджил из 27 ветки. Откуда-то взялась viewModel. Буду фиксить

--- app/src/main/java/com/example/weatherretrofit2/data/WeatherApi.kt
+++ app/src/main/java/com/example/weatherretrofit2/data/WeatherApi.kt
@@ -1,6 +1,5 @@
-package com.example.weatherretrofit2
+package com.example.weatherretrofit2.data
 
-import com.example.weatherretrofit2.data.DataWeatherFromNet
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
@@ -8,10 +7,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
-//doc https://openweathermap.org/forecast5
-//https://api.openweathermap.org/data/2.5/forecast?id=1503901&units=metric&appid=2ce0a504eccbb5cc5fdb54b14b60fab2 для Api
 const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 interface WeatherApi {
     @GET("forecast")
--- app/src/main/java/com/example/weatherretrofit2/fragment/WeatherFragment.kt
+++ app/src/main/java/com/example/weatherretrofit2/fragment/WeatherFragment.kt
@@ -7,17 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.weatherretrofit2.data.DataWeather
-import com.example.weatherretrofit2.data.WeatherAdapter
-import com.example.weatherretrofit2.data.WeatherSealed
 import com.example.weatherretrofit2.databinding.FragmentWeatherBinding
+import com.example.weatherretrofit2.presentation.WeatherAdapter
+import com.example.weatherretrofit2.ui.WeatherUI
 import timber.log.Timber
 
 private const val STATE_PLUS = 0
 private const val KEY: String = "1"
 
 class WeatherFragment : Fragment() {
-
     private var _binding: FragmentWeatherBinding? = null
     private val binding get() = _binding!!
     private val weatherViewModel: WeatherViewModel by viewModels()
@@ -42,7 +40,7 @@ class WeatherFragment : Fragment() {
         initRecyclerView()
     }
 
-    private fun loadDataToAdapter(weatherDomain: List<DataWeather>) {
+    private fun loadDataToAdapter(weatherDomain: List<WeatherUI>) {
         val list = mutableListOf<WeatherSealed>()
         weatherDomain.map { weather ->
             if (weather.temp > STATE_PLUS) {
@@ -55,7 +53,6 @@ class WeatherFragment : Fragment() {
         Timber.d(list.toString())
         weatherAdapter.submitList(list.toMutableList())
     }
-
 
 
     private fun initRecyclerView() {
