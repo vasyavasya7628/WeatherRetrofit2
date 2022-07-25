3ce8addde13cdfaedff948a6f6e498b9847f6940
Vasily7628 <mineok44@gmail.com>
2022-07-25 12:29:24 +0700

    Debug. Не вызывается погода из фрагмента. При перевороте фрагмент создается, хотя фрагмент должен Detach() делать, тк стоит retainInstance(true)

--- app/src/main/java/com/example/weatherretrofit2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherretrofit2/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.weatherretrofit2.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weatherretrofit2.data.WeatherApi
 import com.example.weatherretrofit2.data.WeatherNW
@@ -34,7 +35,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadWeather() {
-        if (weatherFragment.savedList.isNullOrBlank()) {
+        if (weatherFragment.getWeather() == null) {
             loadFromNetwork()
         } else {
             loadFromFragment()
@@ -49,13 +50,14 @@ class MainActivity : AppCompatActivity() {
         ).enqueue(object : retrofit2.Callback<WeatherNW> {
             override fun onResponse(
                 call: Call<WeatherNW>,
-                response: Response<WeatherNW>
+                response: Response<WeatherNW>,
             ) {
                 val weathers: List<WeatherUI> = response.body()?.list?.map { weatherNw ->
                     weatherNw.toDomain()
                 }.orEmpty()
                 saveWeather(Gson().toJson(weathers))
                 weatherAdapter.submitList(weathers)
+                Timber.d("Погода загружена из интернета")
             }
 
             override fun onFailure(call: Call<WeatherNW>, t: Throwable) {
@@ -64,18 +66,27 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    fun saveWeather(list: String){
+    fun saveWeather(list: String) {
+        supportFragmentManager
+            .beginTransaction()
+            .add(weatherFragment, "weatherFragment")
+            .commit()
+
         val bundle = Bundle()
         bundle.putString(KEY, list)
         weatherFragment.arguments = bundle
+        Timber.d("Погода сохранена")
+
+
     }
 
     private fun loadFromFragment() {
         val weathers = Gson().fromJson<List<WeatherUI>>(
-            weatherFragment.savedList,
+            weatherFragment.getWeather(),
             object : TypeToken<List<WeatherUI>>() {}.type
         ).toMutableList()
         weatherAdapter.submitList(weathers)
+        Timber.d("Погода загружена из fraugment")
     }
 
     private fun initRecyclerView() {
--- app/src/main/java/com/example/weatherretrofit2/presentation/WeatherFragment.kt
+++ app/src/main/java/com/example/weatherretrofit2/presentation/WeatherFragment.kt
@@ -6,25 +6,32 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.weatherretrofit2.databinding.FragmentWeatherBinding
+import timber.log.Timber
 
 private const val KEY = "1"
 
 class WeatherFragment : Fragment() {
     private var _binding: FragmentWeatherBinding? = null
     private val binding get() = _binding!!
-    var savedList: String? = null
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        savedList = arguments?.getString(KEY)
-        _binding = FragmentWeatherBinding.inflate(layoutInflater, container, false)
-        return binding.root
-    }
+    private var savedList: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         retainInstance = true
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentWeatherBinding.inflate(layoutInflater, container, false)
+        savedList = arguments?.getString(KEY)
+        Timber.d(savedList)
+        return binding.root
+    }
+
+    fun getWeather(): String? {
+        return savedList
+    }
+
 }
